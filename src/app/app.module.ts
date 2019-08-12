5903270159c6db4fc05a220abff082320743b37e
Sudhir Paturu <sudhir_paturu@epam.com>
2019-08-12 11:28:53 +0530

    Initial Commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { ArticlesModule } from './articles/articles.module';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,17 +10,14 @@ import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WelcomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot() ,
-    HttpClientModule,
-    AppRoutingModule,
-    ArticlesModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
