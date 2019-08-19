d2beae6a2f7b7808e371cb184301f087654a4928
Sudhir Paturu <sudhir_paturu@epam.com>
2019-08-19 21:06:29 +0530

    Task2 commit

---
+++ src/app/articles/article.ts
@@ -0,0 +1,10 @@
+export interface IArticle {
+    source: any;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
--- src/app/articles/articles.service.ts
+++ src/app/articles/articles.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-//import { local, worldwidenews } from '../../../data/articles-data.json';
 import { catchError, tap, map } from 'rxjs/operators';
 
 
@@ -10,18 +9,33 @@ export class ArticlesService {
 
     totalArticles = [];
     currentArticle = {};
-    sourceType: string="";
+    sourceType: string = "";
+    configData = {};
     constructor(private http: HttpClient) {
-
+        this.getConfigData();
     }
 
     getArticles(sourceType, startIndex) {
-        return this.http.get('data/articles-data.json').pipe(
+        return this.http.get(`${this.configData['apiUrl']}/sources?apiKey=${this.configData['apiKey']}`).pipe(
             map((data) => {
                 const nextarticles = data[sourceType].slice(startIndex, startIndex + 5);
                 this.totalArticles = [...this.totalArticles, ...nextarticles]
                 return nextarticles;
             })
         );
+    }
+
+    getSources() {
+        return this.http.get(`${this.configData['apiUrl']}/sources?apiKey=${this.configData['apiKey']}`).pipe(
+            map((data) => {
+                return data;
+            })
+        );
+    }
+
+    getConfigData() {
+        this.http.get('data/appConfig.json').subscribe(data => {
+            this.configData = data;
+        });
     }
 }
